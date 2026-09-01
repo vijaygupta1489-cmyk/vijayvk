c284c7d0ba4fdf6b38b385f2ad8482b903bccdd5
user <user@DESKTOP-EIKPBKQ>
2026-09-01 10:39:59 +0530

    Pracisebean commited

--- TestProj/src/Pracisebean/MainClass.java
+++ TestProj/src/Pracisebean/MainClass.java
@@ -4,7 +4,14 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+StudBean st=new StudBean();
+st.setVANNUMBER(6558);
+st.setVANNAME("Benz");
+st.setVANMODEL("mh02");
 
+System.out.println("VAN Number " +st.getVANNUMBER());
+System.out.println("VAN Name " +st.getVANNAME());
+System.out.println("VANModel " +st.getVANMODEL()) ;
 	}
 
 }
--- TestProj/src/Pracisebean/StudBean.java
+++ TestProj/src/Pracisebean/StudBean.java
@@ -2,5 +2,25 @@ package Pracisebean;
 
 public class StudBean {
 
-	
+	private int VANNUMBER;
+	private String VANNAME;
+	public int getVANNUMBER() {
+		return VANNUMBER;
+	}
+	public void setVANNUMBER(int vANNUMBER) {
+		VANNUMBER = vANNUMBER;
+	}
+	public String getVANNAME() {
+		return VANNAME;
+	}
+	public void setVANNAME(String vANNAME) {
+		VANNAME = vANNAME;
+	}
+	public String getVANMODEL() {
+		return VANMODEL;
+	}
+	public void setVANMODEL(String vANMODEL) {
+		VANMODEL = vANMODEL;
+	}
+	private String VANMODEL;
 }
